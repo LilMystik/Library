4fda361ea1a88d76452f6324ac98ef502a15bd85
LilMystik <waitforfiveminutes@gmail.com>
2024-04-09 10:45:49 +0300

    initial

--- src/main/java/com/library/repository/BookRepository.java
+++ src/main/java/com/library/repository/BookRepository.java
@@ -2,9 +2,16 @@ package com.library.repository;
 
 import com.library.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book,Long> {
-    void deleteBookById(Long id);
+    void deleteBookByTitle(String titleName);
     Book findBookById(Long id);
     Book findBookByTitle(String titleName);
+
+    @Query(value="SELECT * FROM Books WHERE title=:titlename AND list_number=:list",nativeQuery = true)
+    Book findBookByTitleAndAndListNumber(@Param("titlename") String title,@Param("list") int listnumber);
 }
