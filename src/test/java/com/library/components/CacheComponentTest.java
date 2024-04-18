da8f6171ef844f8de3067a18ce93e2fbf3d4f46c
LilMystik <waitforfiveminutes@gmail.com>
2024-04-18 15:54:38 +0300

    initial

--- src/test/java/com/library/components/CacheComponentTest.java
+++ src/test/java/com/library/components/CacheComponentTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
-public class CacheComponentTest {
+class CacheComponentTest {
   private Cache cacheOld;
 
   @BeforeEach
