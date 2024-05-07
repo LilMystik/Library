7a5796915cf99ee8fcdeeaebaca16b4167a8d295
LilMystik <waitforfiveminutes@gmail.com>
2024-05-07 13:41:56 +0300

    initial

---
+++ src/main/java/com/library/service/CounterService.java
@@ -0,0 +1,22 @@
+package com.library.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Service
+public  class  CounterService  {
+  private final AtomicInteger count;
+
+  public  CounterService() {
+    count = new AtomicInteger(0);
+  }
+
+  public int incrementAndGet(){
+    return count.incrementAndGet();
+  }
+
+  public int get() {
+    return count.get();
+  }
+}
